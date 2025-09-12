8d34d9914f43d183ee2dbcd62dfd56a844e88422
Sundaram Tiwari <145829895+TiwariSundaram@users.noreply.github.com>
2025-09-13 02:39:57 +0530

    javaScript code

---
+++ Task1/script1.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const alertButton = document.getElementById('alertButton');
+
+    alertButton.addEventListener('click', () => {
+        alert('Hello! You clicked the button. 🎉');
+    });
+});
