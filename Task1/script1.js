8354cced31b83bb38d6875e0a8db4a430bcbd083
Sundaram Tiwari <145829895+TiwariSundaram@users.noreply.github.com>
2025-09-13 02:47:54 +0530

    Delete Task1/script1.js

--- Task1/script1.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const alertButton = document.getElementById('alertButton');
-
-    alertButton.addEventListener('click', () => {
-        alert('Hello! You clicked the button. 🎉');
-    });
-});
